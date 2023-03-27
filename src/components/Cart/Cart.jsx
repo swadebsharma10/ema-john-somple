f83cb375b1572514b6ae4fece21967d49e429b01
ph-swadeb-07 <swadebsharma2022@gmail.com>
2023-03-28 03:11:30 +0600

    show product quantity on cart

--- src/components/Cart/Cart.jsx
+++ src/components/Cart/Cart.jsx
@@ -7,9 +7,16 @@ const Cart = ({cart}) => {
 
     let totalPrice = 0;
     let totalShipping = 0;
+    let quantity = 0;
     for(const product of cart){
-        totalPrice = totalPrice + product.price;
+        // if(product.quantity ===0){
+        //     product.quantity = 1;
+        // }
+        // product.quantity = product.quantity || 1;
+
+        totalPrice = totalPrice + product.price * product.quantity;
         totalShipping = totalShipping + product.shipping;
+        quantity = quantity + product.quantity;
     }
     const tax = totalPrice*7/ 100;
 
@@ -18,7 +25,7 @@ const Cart = ({cart}) => {
     return (
         <div className='cart'>
             <h3>Order Summery</h3>
-            <p>Selected Items: {cart.length}</p>
+            <p>Selected Items: {quantity}</p>
             <p>Total Price: ${totalPrice}</p>
             <p>Total Shipping: ${totalShipping}</p>
             <p>Tax:${tax.toFixed(2)}</p>
