3e1302b5c959a887b7488411ca0bd4c41c16457f
ph-swadeb-07 <swadebsharma2022@gmail.com>
2023-04-05 14:36:14 +0600

    added order section

--- src/components/Orders/Orders.jsx
+++ src/components/Orders/Orders.jsx
@@ -1,9 +1,27 @@
 import React from 'react';
+import Cart from '../Cart/Cart';
+import { useLoaderData } from 'react-router-dom';
+import ReviewItems from '../ReviewItems/ReviewItems';
+import './Orders.css';
 
 const Orders = () => {
+
+    const cart = useLoaderData();
+    console.log(cart);
+
     return (
-        <div>
-            <h1>Orders Page</h1>
+        <div className='shop-container'>
+              <div className='review-container'>
+                {
+                    cart.map(product => <ReviewItems
+                    key={product.id}
+                    product={product}
+                    ></ReviewItems>)
+                }
+              </div>
+               <div className='cart-container'>
+               <Cart cart ={cart}></Cart>
+               </div>
         </div>
     );
 };
