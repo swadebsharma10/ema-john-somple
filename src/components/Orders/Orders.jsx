b753d9326f49b14d63f9d792e5aff9e7a24289f3
ph-swadeb-07 <swadebsharma2022@gmail.com>
2023-04-06 03:14:22 +0600

    implimented delete ffrom cart

--- src/components/Orders/Orders.jsx
+++ src/components/Orders/Orders.jsx
@@ -1,13 +1,22 @@
-import React from 'react';
+import React, { useState } from 'react';
 import Cart from '../Cart/Cart';
 import { useLoaderData } from 'react-router-dom';
 import ReviewItems from '../ReviewItems/ReviewItems';
 import './Orders.css';
+import { removeFromDb } from '../../utilities/fakedb';
 
 const Orders = () => {
 
-    const cart = useLoaderData();
-    console.log(cart);
+    const savedCart = useLoaderData();
+    const [cart, setCart] = useState(savedCart);
+
+    const handleRemoveFromCart = (id) =>{
+          const remaining = cart.filter(product => product.id !== id);
+          setCart(remaining);
+          removeFromDb(id)
+          
+    }
+    // console.log(savedCart);
 
     return (
         <div className='shop-container'>
@@ -16,6 +25,7 @@ const Orders = () => {
                     cart.map(product => <ReviewItems
                     key={product.id}
                     product={product}
+                    handleRemoveFromCart ={handleRemoveFromCart}
                     ></ReviewItems>)
                 }
               </div>
--- src/components/ReviewItems/ReviewItems.jsx
+++ src/components/ReviewItems/ReviewItems.jsx
@@ -3,7 +3,7 @@ import './ReviewItem.css';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 import { faTrashAlt} from '@fortawesome/free-solid-svg-icons'
 
-const ReviewItems = ({product}) => {
+const ReviewItems = ({product, handleRemoveFromCart}) => {
     const {id, img, price, name, quantity} = product;
     return (
         <div className='review-item'>
@@ -13,7 +13,7 @@ const ReviewItems = ({product}) => {
                 <p>Price:<span className='orange-text'>${price}</span></p>
                 <p>Order-Quantity:<span className='orange-text'>${quantity}</span></p> 
           </div>
-          <button className='btn-delete'>
+          <button onClick={()=>handleRemoveFromCart(id)} className='btn-delete'>
           <FontAwesomeIcon className='delete-icon' icon={faTrashAlt} />
           </button>
         </div>
